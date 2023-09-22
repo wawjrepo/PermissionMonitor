19e19a745c4e987610d9521270bf3d212d656af8
piyouqing <piyouqing@5i5j.com>
2023-09-22 13:56:45 +0800

    增加读取其他app

--- permissionMonitor/src/main/java/com/hua/permissionmonitor/method/NormalMethodList.java
+++ permissionMonitor/src/main/java/com/hua/permissionmonitor/method/NormalMethodList.java
@@ -46,9 +46,10 @@ import java.util.concurrent.Executor;
  * ⠻⣿⣿⣿⣿⣏⠻⢌⣉⣉⣩⣉⡛⣛⠒⠶⠶⠶⠶⠶⠶⠶⠶⠂⣸⣿
  * ⣥⣈⠙⡻⠿⠿⣷⣿⣿⣿⣿⣿⣿⣿⣿⣿⣾⣿⠿⠛⢉⣠⣶⣶⣿⣿
  * ⣿⣿⣿⣶⣬⣅⣒⣒⡂⠈⠭⠭⠭⠭⠭⢉⣁⣄⡀⢾⣿⣿⣿⣿⣿⣿
- *
+ * <p>
  * 危险的监控项目
  * Created on 2021/7/22 9:11.
+ *
  * @author hua
  * @file NormalMethodList
  */
@@ -58,10 +59,23 @@ public class NormalMethodList extends HookMethodList {
     public LinkedList<ClassMethodGroup> getAbsMethodList() {
         LinkedList<ClassMethodGroup> list = new LinkedList<>();
         //PackageManager
-        ClassMethodGroup pkgManagerHook = new ClassMethodGroup("android.app.ApplicationPackageManager");
-        pkgManagerHook.addMethod("getPackageInfoAsUser"); //获取应用信息
-        pkgManagerHook.addMethod("getInstalledPackagesAsUser");
+        ClassMethodGroup aPkgManagerHook = new ClassMethodGroup("android.app.ApplicationPackageManager");
+        aPkgManagerHook.addMethod("getInstalledPackages");
+        aPkgManagerHook.addMethod("getInstalledApplications"); //获取应用信息
+        aPkgManagerHook.addMethod("queryIntentActivities");
+        aPkgManagerHook.addMethod("getInstallerPackageName");
+        aPkgManagerHook.addMethod("getPackageInfoAsUser");
+        list.add(aPkgManagerHook);
+
+        ClassMethodGroup pkgManagerHook = new ClassMethodGroup("android.app.PackageManager");
+        pkgManagerHook.addMethod("getInstalledPackages");
+        pkgManagerHook.addMethod("getInstalledApplications");
         list.add(pkgManagerHook);
+
+        ClassMethodGroup acManagerHook = new ClassMethodGroup("android.app.ActivityManager");
+        acManagerHook.addMethod("getRunningAppProcesses");
+        list.add(acManagerHook);
+
         //SmsManager    短信
         ClassMethodGroup smsManagerHook = new ClassMethodGroup("android.telephony.SmsManager");
         smsManagerHook.addMethod("sendTextMessageInternal");
@@ -83,67 +97,69 @@ public class NormalMethodList extends HookMethodList {
         //------------------------------------
         //TODO 添加数据
         //-------设备信息
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getDeviceId"));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getDeviceId",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getSubscriberId",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getImei"));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getImei",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getMeid",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getSimSerialNumber",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getSimOperatorNameForPhone",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getSimCountryIsoForPhone",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getTypeAllocationCode",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getManufacturerCode",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getNaiBySubscriberId",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getPhoneTypeFromNetworkType",int.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getTelephonyProperty",int.class,String.class,String.class));
-        list.add(new MethodWrapper(android.telephony.TelephonyManager.class,"getTelephonyProperty",String.class,String.class));
-        list.add(new MethodWrapper(Build.class,"getSerial"));
-        list.add(new MethodWrapper(android.net.wifi.WifiInfo.class,"getMacAddress"));
-        list.add(new MethodWrapper(java.net.NetworkInterface.class,"getHardwareAddress"));
-        MethodWrapper s1 = new MethodWrapper(android.provider.Settings.Secure.class,"getString", android.content.ContentResolver.class,String.class);
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getDeviceId"));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getDeviceId", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getSubscriberId", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getImei"));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getImei", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getMeid", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getSimSerialNumber", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getSimOperatorNameForPhone", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getSimCountryIsoForPhone", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getTypeAllocationCode", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getManufacturerCode", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getNaiBySubscriberId", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getPhoneTypeFromNetworkType", int.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getTelephonyProperty", int.class, String.class, String.class));
+        list.add(new MethodWrapper(android.telephony.TelephonyManager.class, "getTelephonyProperty", String.class, String.class));
+        list.add(new MethodWrapper(Build.class, "getSerial"));
+        list.add(new MethodWrapper(android.net.wifi.WifiInfo.class, "getMacAddress"));
+        list.add(new MethodWrapper(android.net.wifi.WifiInfo.class, "getSSID"));
+        list.add(new MethodWrapper(android.net.wifi.WifiInfo.class, "getBSSID"));
+        list.add(new MethodWrapper(java.net.NetworkInterface.class, "getHardwareAddress"));
+        MethodWrapper s1 = new MethodWrapper(android.provider.Settings.Secure.class, "getString", android.content.ContentResolver.class, String.class);
         s1.setMethodHandler(new SettingsResolverHandler());
         list.add(s1);
-        MethodWrapper s2 = new MethodWrapper(Settings.System.class,"getString", android.content.ContentResolver.class,String.class);
+        MethodWrapper s2 = new MethodWrapper(Settings.System.class, "getString", android.content.ContentResolver.class, String.class);
         s2.setMethodHandler(new SettingsResolverHandler());
         list.add(s2);
         //-----定位
-        list.add(new MethodWrapper(android.location.LocationManager.class,"getLastLocation"));
-        list.add(new MethodWrapper(android.location.LocationManager.class,"getLastKnownLocation",String.class));
+        list.add(new MethodWrapper(android.location.LocationManager.class, "getLastLocation"));
+        list.add(new MethodWrapper(android.location.LocationManager.class, "getLastKnownLocation", String.class));
         //--屏幕
-        list.add(new MethodWrapper(android.view.SurfaceControl.class,"screenshot", Rect.class,Integer.TYPE,Integer.TYPE,Boolean.TYPE,Integer.TYPE));
-        list.add(new MethodWrapper(MediaRecorder.class,"start"));
-        list.add(new MethodWrapper(MediaRecorder.class,"prepare"));
-        list.add(new MethodWrapper(ImageReader.class,"acquireLatestImage"));
+        list.add(new MethodWrapper(android.view.SurfaceControl.class, "screenshot", Rect.class, Integer.TYPE, Integer.TYPE, Boolean.TYPE, Integer.TYPE));
+        list.add(new MethodWrapper(MediaRecorder.class, "start"));
+        list.add(new MethodWrapper(MediaRecorder.class, "prepare"));
+        list.add(new MethodWrapper(ImageReader.class, "acquireLatestImage"));
         //--网络
-        list.add(new MethodWrapper(WifiManager.class,"setWifiEnabled",boolean.class));
-        list.add(new MethodWrapper(WifiManager.class,"isWifiEnabled"));
-        list.add(new MethodWrapper(WifiManager.class,"startScan"));
-        list.add(new MethodWrapper(NetworkInterface.class,"getNetworkInterfaces"));
+        list.add(new MethodWrapper(WifiManager.class, "setWifiEnabled", boolean.class));
+        list.add(new MethodWrapper(WifiManager.class, "isWifiEnabled"));
+        list.add(new MethodWrapper(WifiManager.class, "startScan"));
+        list.add(new MethodWrapper(NetworkInterface.class, "getNetworkInterfaces"));
         //--系统特殊处理
-        MethodWrapper contentResolverWrap = new MethodWrapper(ContentResolver.class,"query", Uri.class,String[].class,String.class,String[].class,String.class);
+        MethodWrapper contentResolverWrap = new MethodWrapper(ContentResolver.class, "query", Uri.class, String[].class, String.class, String[].class, String.class);
         contentResolverWrap.setMethodHandler(new ContentResolverHandler());
         list.add(contentResolverWrap);
         //--蓝牙
-        list.add(new MethodWrapper(BluetoothAdapter.class,"enable"));
-        list.add(new MethodWrapper(BluetoothAdapter.class,"startLeScan", UUID[].class, BluetoothAdapter.LeScanCallback.class));
-        list.add(new MethodWrapper(BluetoothLeScanner.class,"startScan",List.class, ScanSettings.class, WorkSource.class, ScanCallback.class, PendingIntent.class,List.class));
-        list.add(new MethodWrapper(BluetoothDevice.class,"createBond"));
-        list.add(new MethodWrapper(BluetoothAdapter.class,"getProfileConnectionState",int.class));
-        list.add(new MethodWrapper(BluetoothAdapter.class,"getBondedDevices"));
+        list.add(new MethodWrapper(BluetoothAdapter.class, "enable"));
+        list.add(new MethodWrapper(BluetoothAdapter.class, "startLeScan", UUID[].class, BluetoothAdapter.LeScanCallback.class));
+        list.add(new MethodWrapper(BluetoothLeScanner.class, "startScan", List.class, ScanSettings.class, WorkSource.class, ScanCallback.class, PendingIntent.class, List.class));
+        list.add(new MethodWrapper(BluetoothDevice.class, "createBond"));
+        list.add(new MethodWrapper(BluetoothAdapter.class, "getProfileConnectionState", int.class));
+        list.add(new MethodWrapper(BluetoothAdapter.class, "getBondedDevices"));
         //--录音
-        list.add(new MethodWrapper(AudioRecord.class,"startRecording"));
-        list.add(new MethodWrapper(MediaRecorder.class,"start"));
-        list.add(new MethodWrapper(MediaRecorder.class,"prepare"));
+        list.add(new MethodWrapper(AudioRecord.class, "startRecording"));
+        list.add(new MethodWrapper(MediaRecorder.class, "start"));
+        list.add(new MethodWrapper(MediaRecorder.class, "prepare"));
         //--拍照
-        list.add(new MethodWrapper(Camera.class,"startPreview"));
-        list.add(new MethodWrapper(CameraManager.class,"openCameraDeviceUserAsync",String.class, CameraDevice.StateCallback.class, Executor.class,int.class));
+        list.add(new MethodWrapper(Camera.class, "startPreview"));
+        list.add(new MethodWrapper(CameraManager.class, "openCameraDeviceUserAsync", String.class, CameraDevice.StateCallback.class, Executor.class, int.class));
         //--剪贴板
-        list.add(new MethodWrapper(ClipboardManager.class,"getPrimaryClip"));
-        list.add(new MethodWrapper(ClipboardManager.class,"getText"));
-        list.add(MethodWrapper.newPrint(ClipboardManager.class,"setText",CharSequence.class));
-        list.add(MethodWrapper.newPrint(ClipboardManager.class,"setPrimaryClip", ClipData.class));
-        list.add(new MethodWrapper(ClipData.class,"getItemAt",int.class));
+        list.add(new MethodWrapper(ClipboardManager.class, "getPrimaryClip"));
+        list.add(new MethodWrapper(ClipboardManager.class, "getText"));
+        list.add(MethodWrapper.newPrint(ClipboardManager.class, "setText", CharSequence.class));
+        list.add(MethodWrapper.newPrint(ClipboardManager.class, "setPrimaryClip", ClipData.class));
+        list.add(new MethodWrapper(ClipData.class, "getItemAt", int.class));
 
 
         //-----------
